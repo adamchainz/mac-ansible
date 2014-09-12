a849c01d35d0419dcf649fc344e20f1000f7299b
Adam Chainz <adam@adamj.eu>
2014-09-12 12:17:59 +0100

    Add profile_default startup for IPython

---
+++ roles/adam_mac/files/dotfiles/ipython/profile_default/startup/00-imports.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from fractions import Fraction
+import datetime as dt
+import math
+import sys
+
+try:
+    import pandas as pd
+except ImportError:
+    pass
+
+try:
+    import numpy as np
+except ImportError:
+    pass
