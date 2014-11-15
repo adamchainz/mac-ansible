0237747b446e37952a2467bc7bbd8192bd551172
Adam Chainz <adam@adamj.eu>
2014-11-15 09:26:36 +0000

    Stop importing pandas/numpy

--- roles/adam_mac/files/dotfiles/ipython/profile_default/startup/00-imports.py
+++ roles/adam_mac/files/dotfiles/ipython/profile_default/startup/00-imports.py
@@ -3,13 +3,3 @@ from fractions import Fraction
 import datetime as dt
 import math
 import sys
-
-try:
-    import pandas as pd
-except ImportError:
-    pass
-
-try:
-    import numpy as np
-except ImportError:
-    pass
